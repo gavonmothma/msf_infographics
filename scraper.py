aafc02c6b0235cc075e7e785020c80f388419705
gavonmothma <mgleason007@gmail.com>
2024-05-13 18:03:48 -0400

    Updated

--- scraper.py
+++ scraper.py
@@ -25,3 +25,23 @@ for toon in characters['data']:
         outfile.write(json_object)
 
 print ("Now run mv ./temp/* ./public/characters/")
+
+
+
+# cd ~/projects/msf/msf_infographics
+
+# Pull new characters.json
+
+# curl -X GET "https://api.marvelstrikeforce.com/game/v1/characters?status=playable" \
+# -H "Accept: application/json" \
+# -H "x-api-key: 17wMKJLRxy3pYDCKG5ciP7VSU45OVumB2biCzzgw" \
+# -H "Authorization: Bearer Ta5dZEdbwBrzNeauv0IkiyYTWCF-LjIXOJg5Zyy1v68.2unchiUqxG6lQdbzqdYRfKm0ggWmhnnsJaw2vBoOoLc"
+# -o characters.json
+
+# cp characters.json ~/projects/msf/msf_infographics/src/data/json/
+
+# python3 scraper.py
+# mv ./temp/* ./public/characters/
+
+# git commit -a
+# git push
