4f6ac34bbd1d12f564ca8568e452da99d0acce32
gavonmothma <mgleason007@gmail.com>
2024-06-27 22:06:30 -0400

    Updated

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ cp ~/projects/msf/new/Config/combat_data/characters.json ~/projects/msf/msf_info
 curl -X GET "https://api.marvelstrikeforce.com/game/v1/characters?status=playable" \
  -H "Accept: application/json" \
  -H "x-api-key: 17wMKJLRxy3pYDCKG5ciP7VSU45OVumB2biCzzgw" \
- -H "Authorization: Bearer Ta5dZEdbwBrzNeauv0IkiyYTWCF-LjIXOJg5Zyy1v68.2unchiUqxG6lQdbzqdYRfKm0ggWmhnnsJaw2vBoOoLc" -o characters.json
+ -H "Authorization: Bearer uLMBos9lsl_0c1ce3XhaefOnhcJ8YaY4168FA6G1NVo.7LX3akcwKcsAmy-GXWqFH3YKJvv0ZFrHs5PkLhMSd50" -o characters.json
 
 cp characters.json ~/projects/msf/msf_infographics/src/data/json/
 
